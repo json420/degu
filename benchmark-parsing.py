9de1ddac8d28823218cbf1bf8fa35b2f80b88883
Jason Gerard DeRose <jason@system76.com>
2014-01-24 23:27:41 -0700

    parser benchmark tweaks

--- benchmark-parsing.py
+++ benchmark-parsing.py
@@ -5,11 +5,19 @@ import timeit
 setup = """
 from degu.client import parse_status
 from degu.server import parse_request
-from degu.base import parse_header
+from degu.base import parse_header, parse_headers
 
 line_bytes = (b'L' *  50) + b'\\r\\n'
 assert line_bytes.endswith(b'\\r\\n')
 assert line_bytes[-2:] == b'\\r\\n'
+
+lines = (
+    'POST /foo/bar?stuff=junk HTTP/1.1',
+    'Content-Type: application/json',
+    'Accept: application/json',
+    'Content-Length: 1234567',
+    'User-Agent: Microfiber/14.04',
+)
 """
 
 
@@ -25,29 +33,27 @@ run("line_bytes.endswith(b'\\r\\n')")
 run("line_bytes[-2:] == b'\\r\\n'")
 run("line_bytes[:-2].decode('latin_1')")
 run("line_bytes[:-2].decode('utf-8')")
+run("line_bytes[:-2].decode()")
 
-print('\nParse request line:')
-run("parse_request('GET /foo/bar?stuff=junk HTTP/1.1')")
+print('\nSplit performance:')
+run("'GET /foo/bar?stuff=junk HTTP/1.1'.split(' ', 2)")
 run("'GET /foo/bar?stuff=junk HTTP/1.1'.split(' ')")
-run("'GET' not in {'GET', 'PUT', 'POST', 'DELETE', 'HEAD'}")
-run("'/foo/bar?stuff=junk'.startswith('/')")
-run("'..' in '/foo/bar?stuff=junk'")
-run("'/foo/bar?stuff=junk'.split('?')")
-run("'/foo/bar'[1:].split('/')")
-
-print('\nParse status line:')
-run("parse_status('HTTP/1.1 404 Not Found')")
-
-print('\nParse Content-Type header:')
-run("parse_header('Content-Type: application/json')")
-run("'Content-Type: application/json'.split(': ', 1)")
-run("'Content-Type'.casefold()")
-
-print('\nParse Content-Length header:')
-run("parse_header('Content-Length: 1234567')")
+run("'GET /foo/bar?stuff=junk HTTP/1.1'.split()")
 run("'Content-Length: 1234567'.split(': ', 1)")
+run("'Content-Length: 1234567'.split(': ')")
+run("'/foo/bar/baz?stuff=junk&hello=nurse'.split('?', 1)")
+run("'/foo/bar/baz?stuff=junk&hello=nurse'.split('?')")
+run("'/foo/bar/baz?stuff=junk&hello=nurse'.find('?')")
+
+print('\nMisc performance:')
 run("'Content-Length'.casefold()")
 run("int('1234567')")
+
+print('\nHigh-level parsers:')
+run("parse_status('HTTP/1.1 404 Not Found')")
+run('parse_request(lines[0])')
+run('parse_headers(lines[1:])')
+
 
 print('-' * 80)
 
