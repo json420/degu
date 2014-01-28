4cc75a5ffcd9af36938bacae59ba0c658a54a3a3
Jason Gerard DeRose <jason@system76.com>
2014-01-28 08:46:28 -0700

    one more attempt at fixing the ssl options unit tests for Python 3.3.4

--- degu/tests/test_client.py
+++ degu/tests/test_client.py
@@ -428,17 +428,13 @@ class TestSSLClient(TestCase):
             'sslctx.protocol must be ssl.{}'.format(base.TLS.name)
         )
 
-        # not (options & ssl.OP_NO_SSLv2)
         # Note: Python 3.3.4 (and presumably 3.4.0) now disables SSLv2 by
-        # default (which is good), but we still want to unit test and make sure
-        # Degu enforces this; see: http://bugs.python.org/issue20207
+        # default (which is good); Degu enforces this (also good), but becaues
+        # we cannot unset the ssl.OP_NO_SSLv2 bit, we can't unit test to check
+        # that Degu enforces this, so for now, we set the bit here so it works
+        # with Python 3.3.3 still; see: http://bugs.python.org/issue20207
         sslctx = ssl.SSLContext(base.TLS.protocol)
-        sslctx.options &= ssl.OP_NO_SSLv2
-        with self.assertRaises(ValueError) as cm:
-            client.SSLClient(sslctx, hostname, port)
-        self.assertEqual(str(cm.exception),
-            'sslctx.options must include ssl.OP_NO_SSLv2'
-        )
+        sslctx.options |= ssl.OP_NO_SSLv2
 
         # not (options & ssl.OP_NO_COMPRESSION)
         sslctx.options |= ssl.OP_NO_SSLv2
