9ebb7567ac651d3922187d8e306b155dec4307dd
Jason Gerard DeRose <jason@system76.com>
2014-05-20 23:08:43 -0600

    Port run-echo-app.py script to 0.5 API

--- run-echo-app.py
+++ run-echo-app.py
@@ -20,7 +20,8 @@ log = logging.getLogger()
 pki = TempPKI(True)
 httpd = TempSSLServer(pki, IPv6_LOOPBACK, None, echo_app)
 client = httpd.get_client()
-response = client.request('GET', '/foo/bar?stuff=junk',
+conn = client.connect()
+response = conn.request('GET', '/foo/bar?stuff=junk',
     {'accept': 'application/json', 'user-agent': 'Microfiber/14.04'}
 )
 print(response.body.read().decode())
