7a1a47f272e3b3a5e0d48c22ce87f4982c141f92
Jason Gerard DeRose <jason@system76.com>
2015-02-19 19:53:36 -0700

    tweak benchmark-namedtuples.py

--- benchmark-namedtuples.py
+++ benchmark-namedtuples.py
@@ -5,27 +5,25 @@ import timeit
 setup = """
 gc.enable()
 
-from collections import namedtuple
 from degu._basepy import Response as Response1
+from degu._basepy import Request as Request1
 from degu._base import Response as Response2
-Request = namedtuple('Request', 'method uri script path query headers')
+from degu._base import Request as Request2
 
-r1 = {
+req0 = {
     'method': 'GET',
     'uri': '/foo/bar?stuff=junk',
     'script': [],
     'path': ['foo', 'bar'],
     'query': 'stuff=junk',
     'headers': {},
+    'body': None,
 }
+req1 = Request1('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {}, None)
+req2 = Request2('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {}, None)
 
-r2 = ('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {})
-r3 = Request('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {})
-
-
-v1 = Response1(200, 'OK', {}, None)
-v2 = Response2(200, 'OK', {}, None)
-
+rsp1 = Response1(200, 'OK', {}, None)
+rsp2 = Response2(200, 'OK', {}, None)
 """
 
 
@@ -35,7 +33,7 @@ def run_iter(statement, n):
         yield t.timeit(n)
 
 
-def run(statement, K=1000):
+def run(statement, K=500):
     n = K * 1000
     # Choose fastest of 10 runs:
     elapsed = min(run_iter(statement, n))
@@ -45,30 +43,28 @@ def run(statement, K=1000):
 
 
 print('-' * 80)
-run("r1['method']")
-run('r2[0]')
-run('r3[0]')
-run('r3.method')
-run("r1['uri']")
-run('r2[1]')
-run('r3[1]')
-run('r3.uri')
 
-print('')
+print('Creating request objects:')
+run("{'method':'GET','uri':'/foo/bar?stuff=junk','script':[],'path':['foo','bar'],'query':'stuff=junk','headers':{},'body':None}")
+run("Request1('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {}, None)")
+run("Request2('GET', '/foo/bar?stuff=junk', [], ['foo', 'bar'], 'stuff=junk', {}, None)")
+
+print('\nAccessing request items:')
+run("req0['method']")
+run('req1[0]')
+run('req1.method')
+run('req2[0]')
+run('req2.method')
+
+print('\nCreating response objects:')
 run("Response1(200, 'OK', {}, None)")
 run("Response2(200, 'OK', {}, None)")
 
-print('')
-run('v1.status')
-run('v1.reason')
-run('v1.headers')
-run('v1.body')
-
-print('')
-run('v2.status')
-run('v2.reason')
-run('v2.headers')
-run('v2.body')
+print('\nAccessing response items')
+run('rsp1[0]')
+run('rsp1.status')
+run('rsp2[0]')
+run('rsp2.status')
 
 print('-' * 80)
 
