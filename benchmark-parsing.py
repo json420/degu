600cd9358402d39f068227cb13b6867618c3bfc9
Jason Gerard DeRose <jason@system76.com>
2015-04-26 16:54:24 -0600

    Update benchmark-parsing.py for parse_request() API change

--- benchmark-parsing.py
+++ benchmark-parsing.py
@@ -7,8 +7,6 @@ gc.enable()
 
 from io import BytesIO
 
-from degu.base import bodies
-
 from degu._base import (
     parse_headers,
     parse_content_length,
@@ -78,10 +76,10 @@ run("parse_range(b'bytes=0-0')")
 run("parse_range(b'bytes=9999999999999998-9999999999999998')")
 
 print('\nRequest parsing:')
-run('parse_request(request, BytesIO(), bodies)')
-run("parse_request(b'GET / HTTP/1.1', BytesIO(), bodies)")
-run("parse_request(b'DELETE /foo/bar?stuff=junk HTTP/1.1', BytesIO(), bodies)")
-run("parse_request(b'GET / HTTP/1.1\\r\\ncontent-length: 17', BytesIO(), bodies)")
+run('parse_request(request, BytesIO())')
+run("parse_request(b'GET / HTTP/1.1', BytesIO())")
+run("parse_request(b'DELETE /foo/bar?stuff=junk HTTP/1.1', BytesIO())")
+run("parse_request(b'GET / HTTP/1.1\\r\\ncontent-length: 17', BytesIO())")
 run("parse_request_line(b'GET / HTTP/1.1')")
 run("parse_request_line(b'DELETE /foo/bar?stuff=junk HTTP/1.1')")
 run("parse_method(b'GET')")
@@ -95,7 +93,7 @@ run("parse_uri(b'/foo/bar')")
 run("parse_uri(b'/foo/bar?stuff=junk')")
 
 print('\nResponse parsing:')
-run("parse_response('GET', response, BytesIO(), bodies)")
+run("parse_response('GET', response, BytesIO())")
 run("parse_response_line(b'HTTP/1.1 200 OK')")
 run("parse_response_line(b'HTTP/1.1 404 Not Found')")
 
