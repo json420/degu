ecaa01aa2894d7b6744461ae6bfb2134f64c86ae
Jason Gerard DeRose <jason@system76.com>
2016-02-27 19:38:17 -0700

    Small cleanup in ContentRange definition in degu/_base.h

--- degu/_base.h
+++ degu/_base.h
@@ -366,41 +366,41 @@ static PyObject * ContentRange_richcompare(ContentRange *, PyObject *, int);
 
 static PyTypeObject ContentRangeType = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "degu._base.ContentRange",                 /* tp_name */
-    sizeof(ContentRange),                      /* tp_basicsize */
-    0,                                  /* tp_itemsize */
-    (destructor)ContentRange_dealloc,          /* tp_dealloc */
-    0,                                  /* tp_print */
-    0,                                  /* tp_getattr */
-    0,                                  /* tp_setattr */
-    0,                                  /* tp_reserved */
-    (reprfunc)ContentRange_repr,               /* tp_repr */
-    0,                                  /* tp_as_number */
-    0,                                  /* tp_as_sequence */
-    0,                                  /* tp_as_mapping */
-    0,                                  /* tp_hash  */
-    0,                                  /* tp_call */
-    (reprfunc)ContentRange_str,                /* tp_str */
-    0,                                  /* tp_getattro */
-    0,                                  /* tp_setattro */
-    0,                                  /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                 /* tp_flags */
-    "ContentRange(start, stop, total)",               /* tp_doc */
-    0,                                  /* tp_traverse */
-    0,                                  /* tp_clear */
-    (richcmpfunc)ContentRange_richcompare,     /* tp_richcompare */
-    0,                                  /* tp_weaklistoffset */
-    0,                                  /* tp_iter */
-    0,                                  /* tp_iternext */
-    0,                                  /* tp_methods */
-    ContentRange_members,                      /* tp_members */
-    0,                                  /* tp_getset */
-    0,                                  /* tp_base */
-    0,                                  /* tp_dict */
-    0,                                  /* tp_descr_get */
-    0,                                  /* tp_descr_set */
-    0,                                  /* tp_dictoffset */
-    (initproc)ContentRange_init,               /* tp_init */
+    "degu._base.ContentRange",              /* tp_name */
+    sizeof(ContentRange),                   /* tp_basicsize */
+    0,                                      /* tp_itemsize */
+    (destructor)ContentRange_dealloc,       /* tp_dealloc */
+    0,                                      /* tp_print */
+    0,                                      /* tp_getattr */
+    0,                                      /* tp_setattr */
+    0,                                      /* tp_reserved */
+    (reprfunc)ContentRange_repr,            /* tp_repr */
+    0,                                      /* tp_as_number */
+    0,                                      /* tp_as_sequence */
+    0,                                      /* tp_as_mapping */
+    0,                                      /* tp_hash  */
+    0,                                      /* tp_call */
+    (reprfunc)ContentRange_str,             /* tp_str */
+    0,                                      /* tp_getattro */
+    0,                                      /* tp_setattro */
+    0,                                      /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                     /* tp_flags */
+    "ContentRange(start, stop, total)",     /* tp_doc */
+    0,                                      /* tp_traverse */
+    0,                                      /* tp_clear */
+    (richcmpfunc)ContentRange_richcompare,  /* tp_richcompare */
+    0,                                      /* tp_weaklistoffset */
+    0,                                      /* tp_iter */
+    0,                                      /* tp_iternext */
+    0,                                      /* tp_methods */
+    ContentRange_members,                   /* tp_members */
+    0,                                      /* tp_getset */
+    0,                                      /* tp_base */
+    0,                                      /* tp_dict */
+    0,                                      /* tp_descr_get */
+    0,                                      /* tp_descr_set */
+    0,                                      /* tp_dictoffset */
+    (initproc)ContentRange_init,            /* tp_init */
 };
 
 
