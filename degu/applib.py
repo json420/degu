8fe19697d2dbf997fe34323d93c71a45638bb920
Jason Gerard DeRose <jason@system76.com>
2016-06-01 23:13:03 -0600

    Doodle on applib.AllowMethods class/decorator, applib.MethodFilter class, and their tests

--- degu/applib.py
+++ degu/applib.py
@@ -24,6 +24,53 @@ A collection of RGI server applications for common scenarios.
 """
 
 
+_METHODS = {'GET', 'PUT', 'POST', 'HEAD', 'DELETE'}
+
+
+class AllowedMethods:
+    __slots__ = ('methods',)
+
+    def __init__(self, *methods):
+        for m in methods:
+            if m not in _METHODS:
+                raise ValueError('bad method: {!r}'.format(m))
+        self.methods = methods
+
+    def __repr__(self):
+        return 'AllowedMethods({})'.format(
+            ', '.join(repr(m) for m in self.methods)
+        )
+
+    def __call__(self, app):
+        return MethodFilter(app, self)
+
+    def isallowed(self, m):
+        return m in self.methods
+
+
+class MethodFilter:
+    __slots__ = ('app', 'allowed_methods')
+
+    def __init__(self, app, allowed_methods):
+        if not callable(app):
+            raise TypeError(
+                'app not callable: {!r}'.format(app)
+            )
+        if type(allowed_methods) is not AllowedMethods:
+            raise TypeError(
+                'allowed_methods: need a {!r}; got a {!r}: {!r}'.format(
+                    AllowedMethods, type(allowed_methods), allowed_methods
+                )
+            )
+        self.app = app
+        self.allowed_methods = allowed_methods
+
+    def __call__(self, session, request, api):
+        if self.allowed_methods.isallowed(request.method):
+            return self.app(session, request, api)
+        return (405, 'Method Not Allowed', {}, None)
+
+
 class RouterApp:
     """
     Generic RGI routing middleware.
--- degu/tests/test_applib.py
+++ degu/tests/test_applib.py
@@ -26,12 +26,222 @@ Unit tests for the `degu.applib` module.
 from unittest import TestCase
 import os
 from collections import OrderedDict
+from random import SystemRandom
 
 from ..base import Request
+from ..misc import mkreq
 from ..sslhelpers import random_id
 from ..misc import TempServer
 from ..client import Client
 from .. import applib
+
+
+random = SystemRandom()
+
+
+METHODS = ('GET', 'PUT', 'POST', 'HEAD', 'DELETE')
+BAD_METHODS = [
+    '',
+    'TRACE',
+    'OPTIONS',
+    'CONNECT',
+    'PATCH',
+    'GOT',
+    'POT',
+    'PUSH',
+    'HELL',
+    'REPEAT',
+]
+BAD_METHODS.extend(m.lower() for m in METHODS)
+BAD_METHODS = tuple(BAD_METHODS)
+ALL_METHODS = METHODS + BAD_METHODS
+
+
+class TestAllowedMethods(TestCase):
+    def test_init(self):
+        # No methods:
+        inst = applib.AllowedMethods()
+        self.assertEqual(inst.methods, tuple())
+
+        # 0 to 5 good methods in random order:
+        for count in range(6):
+            methods = tuple(random.sample(METHODS, count))
+            inst = applib.AllowedMethods(*methods)
+            self.assertEqual(inst.methods, methods)
+
+        # 0 to 5 good methods plus 1 bad method, all in random order:
+        for bad in BAD_METHODS:
+            for count in range(6):
+                methods = random.sample(METHODS, count)
+                methods.append(bad)
+                random.shuffle(methods)
+                with self.assertRaises(ValueError) as cm:
+                    applib.AllowedMethods(*methods)
+                self.assertEqual(str(cm.exception),
+                    'bad method: {!r}'.format(bad)
+                )
+
+    def test_repr(self):
+        # Static value sanity check: no methods
+        inst = applib.AllowedMethods()
+        self.assertEqual(repr(inst), 'AllowedMethods()')
+
+        # Static value sanity check: one or more methods:
+        inst = applib.AllowedMethods('HEAD')
+        self.assertEqual(repr(inst), "AllowedMethods('HEAD')")
+        inst = applib.AllowedMethods('POST', 'DELETE')
+        self.assertEqual(repr(inst), "AllowedMethods('POST', 'DELETE')")
+        inst = applib.AllowedMethods('PUT', 'HEAD', 'GET')
+        self.assertEqual(repr(inst), "AllowedMethods('PUT', 'HEAD', 'GET')")
+        inst = applib.AllowedMethods('GET', 'DELETE', 'POST', 'HEAD')
+        self.assertEqual(repr(inst),
+            "AllowedMethods('GET', 'DELETE', 'POST', 'HEAD')"
+        )
+        inst = applib.AllowedMethods('GET', 'PUT', 'POST', 'HEAD', 'DELETE')
+        self.assertEqual(repr(inst),
+            "AllowedMethods('GET', 'PUT', 'POST', 'HEAD', 'DELETE')"
+        )
+
+        # 0 to 5 good methods in random order:
+        for count in range(6):
+            methods = tuple(random.sample(METHODS, count))
+            inst = applib.AllowedMethods(*methods)
+            self.assertEqual(repr(inst),
+                '{}({})'.format(
+                    inst.__class__.__name__,
+                    ', '.join(repr(m) for m in methods)
+                )
+            )
+
+    def test_call(self):
+        def app(session, request, api):
+            return (200, 'OK', {}, None)
+
+        for count in range(6):
+            methods = tuple(random.sample(METHODS, count))
+            inst = applib.AllowedMethods(*methods)
+
+            # app not callable:
+            bad = 'my_app'
+            with self.assertRaises(TypeError) as cm:
+                inst(bad)
+            self.assertEqual(str(cm.exception),
+                'app not callable: {!r}'.format(bad)
+            )
+
+            # All good:
+            method_filter = inst(app)
+            self.assertIs(type(method_filter), applib.MethodFilter)
+            self.assertIs(method_filter.app, app)
+            self.assertIs(method_filter.allowed_methods, inst)
+
+    def test_isallowed(self):
+        inst = applib.AllowedMethods('POST')
+        self.assertIs(inst.isallowed('POST'), True)
+        self.assertIs(inst.isallowed('GET'), False)
+        self.assertIs(inst.isallowed('PUT'), False)
+        self.assertIs(inst.isallowed('HEAD'), False)
+        self.assertIs(inst.isallowed('DELETE'), False)
+
+        inst = applib.AllowedMethods('PUT', 'HEAD')
+        self.assertIs(inst.isallowed('HEAD'), True)
+        self.assertIs(inst.isallowed('PUT'), True)
+        self.assertIs(inst.isallowed('POST'), False)
+        self.assertIs(inst.isallowed('GET'), False)
+        self.assertIs(inst.isallowed('DELETE'), False)
+
+        for count in range(6):
+            methods = tuple(random.sample(METHODS, count))
+            inst = applib.AllowedMethods(*methods)
+            for m in BAD_METHODS:
+                result = (True if m in methods else False)
+                self.assertIs(inst.isallowed(m), result)
+
+
+class TestMethodFilter(TestCase):
+    def test_init(self):
+        def app(session, request, api):
+            return (200, 'OK', {}, None)
+
+        allowed_methods = applib.AllowedMethods('GET', 'HEAD')
+
+        # app not callable:
+        bad = 'my_app'
+        with self.assertRaises(TypeError) as cm:
+            applib.MethodFilter(bad, allowed_methods)
+        self.assertEqual(str(cm.exception),
+            'app not callable: {!r}'.format(bad)
+        )
+
+        # allowed_methods isn't an AllowedMethods instance:
+        bad = frozenset(['GET', 'HEAD'])
+        with self.assertRaises(TypeError) as cm:
+            applib.MethodFilter(app, bad)
+        self.assertEqual(str(cm.exception),
+            'allowed_methods: need a {!r}; got a {!r}: {!r}'.format(
+                applib.AllowedMethods, type(bad), bad
+            )
+        )
+
+        # All good:
+        inst = applib.MethodFilter(app, allowed_methods)
+        self.assertIs(type(inst), applib.MethodFilter)
+        self.assertIs(inst.app, app)
+        self.assertIs(inst.allowed_methods, allowed_methods)
+
+    def test_call(self):
+        class App:
+            def __init__(self, marker):
+                self.__marker = marker
+
+            def __call__(self, session, request, api):
+                return (200, 'OK', {}, self.__marker)
+
+        marker = os.urandom(16)
+        app = App(marker)
+
+        # No methods allowed:
+        allowed_methods = applib.AllowedMethods()
+        inst = applib.MethodFilter(app, allowed_methods)
+        for m in ALL_METHODS:
+            self.assertEqual(inst(None, mkreq(m, '/'), None),
+                (405, 'Method Not Allowed', {}, None)
+            )
+
+        # One method allowed:
+        for allowed in METHODS:
+            allowed_methods = applib.AllowedMethods(allowed)
+            inst = applib.MethodFilter(app, allowed_methods)
+            for m in METHODS:
+                request = mkreq(m, '/')
+                response = inst(None, request, None)
+                if m == allowed:
+                    self.assertEqual(response,
+                        (200, 'OK', {}, marker)
+                    )
+                else:
+                    self.assertEqual(response,
+                        (405, 'Method Not Allowed', {}, None)
+                    )
+            for m in BAD_METHODS:
+                self.assertEqual(inst(None, mkreq(m, '/'), None),
+                    (405, 'Method Not Allowed', {}, None)
+                )
+
+        # All *good* methods allowed:
+        good = list(METHODS)
+        random.shuffle(good)
+        random.shuffle(good)
+        allowed_methods = applib.AllowedMethods(*good)
+        inst = applib.MethodFilter(app, allowed_methods)
+        for m in METHODS:
+            self.assertEqual(inst(None, mkreq(m, '/'), None),
+                (200, 'OK', {}, marker)
+            )
+        for m in BAD_METHODS:
+            self.assertEqual(inst(None, mkreq(m, '/'), None),
+                (405, 'Method Not Allowed', {}, None)
+            )
 
 
 class TestRouterApp(TestCase):
